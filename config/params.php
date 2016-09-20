ee7ab646fb6b5930701690a163d91e4bc5cb6bfb
tmazza <tiagomdepaula@gmail.com>
2016-09-20 20:54:22 -0300

    Arquivo de parametros com dados AWS

--- config/params.php
+++ config/params.php
@@ -7,4 +7,13 @@ return [
     'db_pass' => '',
     'db_host' => '',
     'db_base' => '',
+	'amazon' => [
+		'bucketUrl' => '',
+		'bucketId' => '',
+		'region' => '',
+		'credentials' => [
+			'key'    => '',
+			'secret' => '',
+		],
+	],
 ];
