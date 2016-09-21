afbe38d866dfba380c3d77d2d8b15c600274bfa0
tmazza <tiagomdepaula@gmail.com>
2016-09-20 23:32:08 -0300

    Página do produto

--- views/site/produto.php
+++ views/site/produto.php
@@ -2,14 +2,22 @@
 use yii\helpers\Html;
 ?>
 <div class="container" style="text-align:center">
+	<br>
 	<h3 style="color:<?=$color?>;">
 		<?=$produto->nome;?>
 	</h3>
-
+	<br>
+	<br>
+	<?php
+	$total = count($produto->imagens);
+	$parcial = 0;
+	?>
 	<?php foreach ($produto->imagens as $i): ?>
+		<?php $parcial++; ?>
 		<?= Html::img($i->getUrl(), [
 			'alt' => 'Imagem ' . $produto->nome,
 		]); ?>
+		<p><?= 'Imagem ' . $parcial.' de ' . $total; ?></p>
 	<?php endforeach; ?>
 
 </div>
